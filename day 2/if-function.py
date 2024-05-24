268af401cfb2a4f88d1a429b4ac7582c0501546c
elyzanst <elynurhalizahnst@gmail.com>
2024-05-24 21:02:59 +0700

    Menyelesaikan Day 2

---
+++ day 2/if-function.py
@@ -0,0 +1,31 @@
+#satu pilihan
+#umur = input("masukkan umur anda = ")
+# if int(umur) >= 17 :
+#     print("Bisa Bikin KTP")
+    
+# print("int Di eksekusi")
+
+#dua pilihan
+# if int(umur) >= 17 :
+#     print("bisa bikin ktp")
+# else :
+#     print("belum bisa bikin ktp")
+
+# #contoh dua
+# password = "kucing"
+# input_password = input("Masukkan Password Anda : ")
+
+# if input_password == password :
+#     print("Password anda benar")
+# else :
+#     print("Passord anda salah")
+
+#tiga pilihan
+barang = "pulsa"
+
+if barang == "mie" :
+    print("saya beli mie")
+elif barang == "telur":
+    print("saya beli telur")
+else :
+    print("dahlah pulang aja")
---
+++ day 2/latihan.py
@@ -0,0 +1,32 @@
+# #latihan if function
+
+# nilai = input("Masukkan nilai anda : ")
+
+# if int (nilai) >= 80 and int(nilai) <=100 :
+#     print("A")
+# elif int(nilai) >= 70  and int(nilai) <=80:
+#     print("B")
+# elif int (nilai) >= 60 and int(nilai) <=70:
+#     print("C")
+# elif int(nilai) >= 50 and int(nilai) <= 60 :
+#     print ("D")
+# elif int (nilai) > 100 or int(nilai) < 0:
+#     print("tidak valid")
+# else :
+#     print("E")
+
+nilai = int(input("Masukkan nilai anda : "))
+
+#konversi
+if nilai > 100 or nilai < 0:
+    print("Nilai Tidak valid")
+elif nilai >= 80:
+    print("Nilai Anda Adalah " + str (nilai) + "Predikat Nilai Anda : A")
+elif nilai >= 70 and nilai < 80 :
+    print("Nilai Anda Adalah " + str (nilai) + "Predikat Nilai Anda : B")
+elif nilai >= 60 and nilai < 70 :
+    print("Nilai Anda Adalah " + str (nilai) + "Predikat Nilai Anda : C")
+elif nilai >= 50 and nilai < 60:
+    print("Nilai Anda Adalah " + str (nilai) + "Predikat Nilai Anda : D")
+else :
+    print("Nilai Anda Adalah " + str (nilai) + "Predikat Nilai Anda : E")
