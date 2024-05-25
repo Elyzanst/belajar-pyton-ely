1c11af0150103efd38fd2f3fdda547c96a9ae00d
elyzanst <elynurhalizahnst@gmail.com>
2024-05-25 20:57:47 +0700

    Menyelesaikan_day-3

---
+++ day-3/function.py
@@ -0,0 +1,12 @@
+def sapaSaya() :
+    print("Selamat Malam Ely")
+    
+#Memanggil Function
+sapaSaya()
+
+#Fungsi dengan parameter
+def luas_persegi(sisi):
+    hasil = sisi * sisi
+    print("luas dari persegi adalah: "+ str(hasil)+"cm^2 \n")
+    
+luas_persegi(5)
---
+++ day-3/looping.py
@@ -0,0 +1,25 @@
+#looping
+for i in range(1,11):
+    print(i)
+    
+#Menampilkan data dari array
+list_siswa = ["Irfan","Yoga","Lenggga","Ely","Nabila"]
+
+for siswa in list_siswa:
+    print(siswa)
+    
+#Menampilkan kalimat secara berulang dengan range
+kalimat = "saya berjanji tidak mengulangi lagi"
+
+for i in range (1,2):
+    print(kalimat)
+    
+#Menampilkan bilangan ganjil
+for i in range(1,30,2):
+    print((i), end=" ")
+    
+print("\n")
+#Menampilkan bilangan genap
+for i in range(1,30,2):
+    print((i)+1, end=" ")
+    
